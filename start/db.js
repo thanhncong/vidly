4e31097ff17f7950a0c41afac4d950cb4aa3db38
danh <ncdanhvn@gmail.com>
2023-07-11 14:26:35 +0700

    prepare the app before deploy to cyclic

--- start/db.js
+++ start/db.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 const logger = require("../start/logging");
 const config = require("config");
 
-module.exports = function () {
+module.exports = async function () {
   const db = config.get("db");
-  mongoose.connect(db).then(() => {
-    logger.info(`Connected to ${db}`);
-  });
+  await mongoose.connect(db); // To make sure databased is connected before start app listening
+  logger.info(`Connected to ${db}`);
 };
